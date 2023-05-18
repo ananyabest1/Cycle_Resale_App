644f98180d113569ed560c7661c4a09e95552372
ananyabest1 <ananyasinghfl21@gmail.com>
2023-05-18 23:46:32 +0530

    Commit

--- lib/screens/home_screen .dart
+++ lib/screens/home_screen .dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -12,16 +13,14 @@ class _HomeScreenState extends State<HomeScreen> {
   late User loggedInUser;
   late String messageText;
 
-
   @override
   void initState() {
     super.initState();
-
     getCurrentUser();
   }
 
   void getCurrentUser() async {
-    try{
+    try {
       final user = await _auth.currentUser!;
       if (user != null) {
         loggedInUser = user;
@@ -31,20 +30,56 @@ class _HomeScreenState extends State<HomeScreen> {
       print(e);
     }
   }
-  //void getmessages() async{
-  //  final messages = await _firestore.collection('messages').get();
-  // for (var message in messages.docs){
-  //print(message.data);
-  // }
-  // }
+
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.calendarAlt,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.user,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      );
+      body: Center(
+        child: Text(
+          "Home",
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: EdgeInsets.only(
+          left: 12,
+          right: 12,
+          bottom: 24,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < navigationIcons.length; i++) ...[
+                Expanded(
+                  child: Center(
+                    child: Icon(navigationIcons[i]),
+                  ),
+                ),
+              ]
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
-
-
